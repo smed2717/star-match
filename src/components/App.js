2612177701e722f012b269131ea9305d6d6f891d
MOHAMED SMAÏRI <smairimohamed@gmail.com>
2022-10-02 14:29:25 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import  React ,{useState}from 'react';
+import Game from './Game';
+
+const StarMatch = () => {
+	const [gameId, setGameId] = useState(1);
+	return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>;
+}
+
+
+export function App({  }) {
+  const [count, setCount] = React.useState(0);
+  return (
+   <StarMatch />
+  );
+}
+
+
+
+
+// import React, { useState } from 'react';
+
+// import Game from './Game';
+
+// const StarMatch = () => {
+//   const [gameId, setGameId] = useState(1);
+//   return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)} />;
+// };
+
+// export default StarMatch;
